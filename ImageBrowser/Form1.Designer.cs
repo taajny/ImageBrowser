745aa68ca174662d95a0a10cff8a5d020a660aa5
Krzysztof <taajny@gmail.com>
2023-09-26 00:12:40 +0200

    Dodaj pliki projektów.

---
+++ ImageBrowser/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ImageBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdOpenPicture = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenPicture = new System.Windows.Forms.Button();
+            this.pbxMainPicture = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNextImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMainPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenPicture
+            // 
+            this.btnOpenPicture.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnOpenPicture.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnOpenPicture.Location = new System.Drawing.Point(167, 4);
+            this.btnOpenPicture.Name = "btnOpenPicture";
+            this.btnOpenPicture.Size = new System.Drawing.Size(258, 73);
+            this.btnOpenPicture.TabIndex = 0;
+            this.btnOpenPicture.Text = "Otwórz plik";
+            this.btnOpenPicture.UseVisualStyleBackColor = true;
+            this.btnOpenPicture.Click += new System.EventHandler(this.btnOpenPicture_Click);
+            // 
+            // pbxMainPicture
+            // 
+            this.pbxMainPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbxMainPicture.Location = new System.Drawing.Point(7, 83);
+            this.pbxMainPicture.Name = "pbxMainPicture";
+            this.pbxMainPicture.Size = new System.Drawing.Size(842, 361);
+            this.pbxMainPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxMainPicture.TabIndex = 1;
+            this.pbxMainPicture.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnClose.Location = new System.Drawing.Point(431, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(258, 73);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Zamknij plik";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPrevious.Location = new System.Drawing.Point(7, 4);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(77, 73);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "<<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Visible = false;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNextImage
+            // 
+            this.btnNextImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNextImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNextImage.Location = new System.Drawing.Point(772, 4);
+            this.btnNextImage.Name = "btnNextImage";
+            this.btnNextImage.Size = new System.Drawing.Size(77, 73);
+            this.btnNextImage.TabIndex = 4;
+            this.btnNextImage.Text = ">>";
+            this.btnNextImage.UseVisualStyleBackColor = true;
+            this.btnNextImage.Visible = false;
+            this.btnNextImage.Click += new System.EventHandler(this.btnNextImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 450);
+            this.Controls.Add(this.btnNextImage);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pbxMainPicture);
+            this.Controls.Add(this.btnOpenPicture);
+            this.Name = "Form1";
+            this.Text = "Przeglądarka obrazów";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMainPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdOpenPicture;
+        private System.Windows.Forms.Button btnOpenPicture;
+        private System.Windows.Forms.PictureBox pbxMainPicture;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNextImage;
+    }
+}
+
---
+++ ImageBrowser/Form1.cs
@@ -0,0 +1,87 @@
+﻿using ImageBrowser.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ImageBrowser
+{
+    public partial class Form1 : Form
+    {
+        private List<string> _listOfFiles = new List<string>();
+        private int _positionOnList = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ofdOpenPicture.Title = "Wybierz proszę plik graficzny do wyświetlenia:";
+            ofdOpenPicture.Filter = "JPG (*.jpg,*.jpeg)|*.jpg;*.jpeg";
+
+            if (!string.IsNullOrWhiteSpace(Settings.Default.LastOpenedFile))
+            {
+                ofdOpenPicture.InitialDirectory = Path.GetDirectoryName(Settings.Default.LastOpenedFile);
+                DisplayPicture(Settings.Default.LastOpenedFile);
+
+            }
+            else
+            {
+                btnClose.Visible = false;
+                ofdOpenPicture.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            }
+        }
+
+        private void btnOpenPicture_Click(object sender, EventArgs e)
+        {
+            if (ofdOpenPicture.ShowDialog() == DialogResult.OK)
+            {
+                DisplayPicture(ofdOpenPicture.FileName);
+                ofdOpenPicture.InitialDirectory = Path.GetDirectoryName(ofdOpenPicture.FileName);
+            }
+        }
+
+        private void DisplayPicture(string filePatch)
+        {
+            pbxMainPicture.Show();
+            pbxMainPicture.ImageLocation = filePatch;
+            pbxMainPicture.Load();
+            btnClose.Visible = true;
+
+            _listOfFiles = Directory.GetFiles(Path.GetDirectoryName(filePatch), "*.jp*g").ToList();
+            _positionOnList = _listOfFiles.FindIndex(f => f.Equals(filePatch));
+            
+            if (_positionOnList > 0)
+                btnPrevious.Visible = true;
+            else
+                btnPrevious.Visible = false;
+
+            if (_positionOnList < _listOfFiles.Count() - 1)
+                btnNextImage.Visible = true;
+            else
+                btnNextImage.Visible = false;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if ( string.IsNullOrWhiteSpace(pbxMainPicture.ImageLocation))
+                Settings.Default.LastOpenedFile = string.Empty;
+            else
+                Settings.Default.LastOpenedFile = pbxMainPicture.ImageLocation;
+            Settings.Default.Save();
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            pbxMainPicture.ImageLocation = string.Empty;
+            pbxMainPicture.Hide();
+            btnClose.Visible = false;
+        }
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            DisplayPicture(_listOfFiles[_positionOnList - 1].ToString());
+        }
+
+        private void btnNextImage_Click(object sender, EventArgs e)
+        {
+            DisplayPicture(_listOfFiles[_positionOnList + 1].ToString());
+        }
+    }
+}
